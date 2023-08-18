0a0353b58bb7e418a1b5125f8f171e30ae0ad7b7
CrackTC <chenrke369@gmail.com>
2023-08-18 11:39:50 +0800

    code clean

--- main.go
+++ main.go
@@ -5,17 +5,17 @@ import (
 	"strconv"
 
 	"sora.zip/blog/config"
+	"sora.zip/blog/router/api"
 	"sora.zip/blog/router/article"
 	"sora.zip/blog/router/index"
 	"sora.zip/blog/router/static"
-	"sora.zip/blog/router/api"
 )
 
 func main() {
 	http.HandleFunc("/", index.Handle)
-	http.HandleFunc("/static/", static.Handle)
-	http.HandleFunc("/article/", article.Handle)
 	http.HandleFunc("/api/", api.Handle)
+	http.HandleFunc("/article/", article.Handle)
+	http.HandleFunc("/static/", static.Handle)
 	err := http.ListenAndServe(":"+strconv.Itoa(config.Get().Port), nil)
 	if err != nil {
 		panic(err)
